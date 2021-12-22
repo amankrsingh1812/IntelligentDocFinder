971b0e97928ddb72ddcaac5425822a0c93b2dfc8
Shivansh Mishra <shivansh18@iitg.ac.in>
2021-12-22 13:54:37 +0000

    Add CLI prototype

--- index.py
+++ index.py
@@ -1,3 +1,30 @@
+from executors.document_preprocessing_executor import insert_document
+from executors.query_executor import execute_query
+import os
+
+
+if __name__ == '__main__':
+    while 1:
+        os.system('clear')
+        print('Enter number for operation:')
+        print('[1] Add document to Intelligent Doc Finder')
+        print('[2] Find relevant document with your query')
+        print('[0] Exit application')
+        operation = int(input())
+        
+        if operation == 1:                    # Add document
+            file_path = input('Enter file path')
+            extension = input('Enter file type')
+            insert_document(file_path, extension)
+            
+        elif operation == 2:                  # Perform query
+            query = input('Enter query')
+            results = execute_query(query)
+            print(results)
+            
+        else:
+            break
+             
 """
 TODOs 
 
@@ -11,35 +38,3 @@ TODOs
 4. Replace naive tokenizer with RegEx based tockenizer
    Ref - https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
 """
-
-
-# url = "https://en.wikipedia.org/wiki/Information_retrieval"
-# html = urlopen(url).read()
-
-# soup = BeautifulSoup(open(generate_filename('html')), "html.parser")
-# txtFileObj = soup.get_text('\n')
-
-# print(txtFileObj)
-
-# # importing required modules
-# import PyPDF2
- 
-# # creating a pdf file object
-# pdfFileObj = open('samples/sample_2_pdf.pdf', 'rb')
- 
-# # creating a pdf reader object
-# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
- 
-# # printing number of pages in pdf file
-# print(pdfReader.numPages)
- 
-# # creating a page object
-# pageObj = pdfReader.getPage(0)
- 
-# # extracting text from page
-# text = pageObj.extractText()
-# print(text)
-# print(type(text))
- 
-# # closing the pdf file object
-# pdfFileObj.close()
